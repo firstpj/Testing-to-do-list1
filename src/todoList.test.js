02e71e3163099df0b7d9b7310397c69001a35719
unknown <albertkantwi@gmail.com>
2023-07-28 18:29:11 +0100

    Fix linters error

--- src/todoList.test.js
+++ src/todoList.test.js
@@ -1,17 +1,17 @@
 import {
-  addItem, removeItem, getTodoList
+  addItem, removeItem, getTodoList,
 } from './todoList.js';
-import showToDoList from './show.js';
+// import showToDoList from './show.js';
 
-describe ('test for crud operations', () => {
+describe('test for crud operations', () => {
   test('addItem,', () => {
     const initialQuantity = getTodoList().length;
     addItem('item1');
     expect(getTodoList().length).toBe(initialQuantity + 1);
-  })
-test('removeItem,', () => {
+  });
+  test('removeItem,', () => {
     const initialQuantity = getTodoList().length;
     removeItem(0);
     expect(getTodoList().length).toBe(initialQuantity - 1);
-  })
-})  
+  });
+});
